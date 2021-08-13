1327f62feff8efb98eaff8ddff1d7718dcc5ef6a
Javier Gonzalez <javier.e.glz@gmail.com>
2021-08-13 15:46:11 -0500

    Buy coin [ch26]: make compilable with abigen go

--- contracts/CreatorToken.sol
+++ contracts/CreatorToken.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "./Stakeable.sol";
 import { Sqrt } from "./Math.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
  * @title CreatorToken
