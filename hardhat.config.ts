0fed23eb4ed2daf519039cfed926d948afd8f9b6
Javier Gonzalez <javier.e.glz@gmail.com>
2021-08-29 18:47:48 -0500

    literally useless edit of accounts hardhat custom code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    const accountBalance = await account.getBalance()
+    console.log(account.address, "balance:", hre.ethers.utils.formatEther(accountBalance));
   }
 });
 
