91f9b0ff68686361fcf744e7992441fda964bd82
Javier Gonzalez <javier.e.glz@gmail.com>
2021-11-03 10:15:10 -0400

    Deploy and set roles

--- test/VestedToken.ts
+++ test/VestedToken.ts
@@ -6,17 +6,17 @@ import { getAddress } from "@ethersproject/address";
 
 chai.use(solidity);
 const { expect } = chai;
-const TEN_MILLION_IN_WEI = ethers.BigNumber.from("10000000000000000000000") as BigNumber;
+const TEN_MILLION_IN_WEI = ethers.BigNumber.from("100000000000000000000000000") as BigNumber;
 const ONE_FINNEY = ethers.utils.parseUnits("1.0", "finney") as BigNumber;
 const ONE_ETH = ethers.utils.parseUnits("1.0", "ether") as BigNumber;
 
 
 describe("VestedToken", () => {
-  let VestedToken: any, token: Contract, owner: Signer, addr1: Signer, addresses: Signer[]
+  let VestedToken: any, token: Contract, deployer: Signer, admin: Signer, safe: Signer, addresses: Signer[]
   const setupVestedToken = async () => {
-    [owner, addr1, ...addresses] = await ethers.getSigners();
+    [deployer, admin, safe, ...addresses] = await ethers.getSigners();
     VestedToken = await ethers.getContractFactory("VestedToken");
-    token = await VestedToken.deploy(TEN_MILLION_IN_WEI, "OrgToken", "TST", await owner.getAddress());
+    token = await VestedToken.deploy("OrgToken", "TST", TEN_MILLION_IN_WEI, await safe.getAddress(), await admin.getAddress());
     await token.deployed();
   }
 
@@ -27,9 +27,21 @@ describe("VestedToken", () => {
       expect(await token.name()).to.equal("OrgToken");
       expect(await token.symbol()).to.equal("TST");
     });
+
+    it("Should launch with the total supply given as a parameter in the constructor to the safe", async () => {
+      expect(await token.totalSupply()).to.equal(TEN_MILLION_IN_WEI);
+      expect(await token.balanceOf(await safe.getAddress())).to.equal(TEN_MILLION_IN_WEI);
+    });
+
+    it("Should give admin access to parameter in the constructor", async () => {
+      await token.connect(admin).mint(await deployer.getAddress(), TEN_MILLION_IN_WEI),
+      expect(await token.totalSupply()).to.equal(TEN_MILLION_IN_WEI.mul(ethers.BigNumber.from(2)));
+    });
+    it("Should only let admin mint new tokens", async () => {
+      await expect(
+        token.connect(deployer)
+        .mint(await deployer.getAddress(), TEN_MILLION_IN_WEI)
+      ).to.be.revertedWith("Admin Role required to call");
+    });
   })
 });
-
-const floatDifferenceIsWithinDelta = (floatOne: number, floatTwo: number, delta = 0.001) : Boolean => {
-  return Math.abs(floatOne - floatTwo) < delta;
-}
