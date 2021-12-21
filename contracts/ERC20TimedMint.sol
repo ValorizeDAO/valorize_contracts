6c205e081cfd205ceb45c19298b5bd1701644c7e
Marco Huberts <marcohuberts@iMac-van-Marco-2.local>
2021-12-21 21:21:51 +0100

    TimedMint + feedback implemented

--- contracts/ERC20TimedMint.sol
+++ contracts/ERC20TimedMint.sol
@@ -13,7 +13,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ERC20TimedMint is ERC20 {
 
     uint256 public timeUntilNextMint;
-    uint256 public claimPeriod;
     uint256 private mintCap;
 
     event Claim(address claimant, uint256 amount);
