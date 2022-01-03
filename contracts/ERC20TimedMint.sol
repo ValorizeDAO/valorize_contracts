e2d4e37539f3e8fe6ab7ed8b4b6243e7878152a6
Marco Huberts <marcohuberts@iMac-van-Marco-2.local>
2022-01-03 20:17:47 +0100

    functions internalized

--- contracts/ERC20TimedMint.sol
+++ contracts/ERC20TimedMint.sol
@@ -1,41 +1,28 @@
 //SPDX-License-Identifier: Unlicense
 
-pragma solidity 0.8.6;
+pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /**
  * @title ERC20TimedMint
  * @author Javier Gonzalez and Marco Huberts
  * @dev Implementation of minting functionality for a mintable token.
- * @notice ERC20TimedMint inherits the ERC20 functionality and prevents 
+ * @notice ERC20TimedMint inherits the ERC20 functionality and prevents
   *         minting within a timeframe.
   */
 contract ERC20TimedMint is ERC20 {
 
     uint256 public timeUntilNextMint;
     uint256 internal mintCap;
-    uint256 internal timeDelay; 
+    uint256 internal timeDelay;
 
-    event Claim(address claimant, uint256 amount);
-    
     constructor (
         string memory name,
         string memory symbol
-    )   
-        ERC20(name, symbol) 
+    )
+        ERC20(name, symbol)
     {
         timeUntilNextMint = block.timestamp + timeDelay;
-    }
-
-    function claimTokens (
-        uint256 amount, 
-        address _receiver
-    ) 
-        external 
-    {
-        require(_receiver == msg.sender); 
-        _transfer(address(this), msg.sender, amount);
-        emit Claim(msg.sender, amount);
     }
 
     function _mint(address to, uint256 amount)
@@ -47,13 +34,11 @@ contract ERC20TimedMint is ERC20 {
         super._mint(to, amount);
     }
 
-    function _setTimeUntilNextMint(uint256 _timeDelay) external {
+    function _setTimeUntilNextMint(uint256 _timeDelay) internal {
         timeDelay = _timeDelay;
     }
 
-    function _setMintCap (uint256 _mintCap) external {
+    function _setMintCap (uint256 _mintCap) internal {
         mintCap = _mintCap;
     }
 }
-
-
